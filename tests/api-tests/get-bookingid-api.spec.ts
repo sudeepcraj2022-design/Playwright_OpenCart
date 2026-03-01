de11c01762e7851db20998fb7a37ac596e15da50
Sudeep Raj <sudeepcraj2022@gmail.com>
2026-03-01 20:07:01 +0530

    Commiting api test2

---
+++ tests/api-tests/get-bookingid-api.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect } from "@playwright/test";
+
+test('Get Booking details by ID - path parameter', async({request}) => {
+
+    const bookingID = 1; //pass booking ID as path parameter
+    const response = await request.get(`https://restful-booker.herokuapp.com/booking/${bookingID}`);
+    
+    //Parse response and print
+    const responseBody = await response.json();
+    console.log('Get Response:', responseBody);
+
+    //Add assertions
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
+});
