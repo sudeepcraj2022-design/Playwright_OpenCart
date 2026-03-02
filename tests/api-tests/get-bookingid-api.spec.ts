19b99955eb17997baa1926fc3ab722c796a8427a
Sudeep Raj <sudeepcraj2022@gmail.com>
2026-03-02 10:22:58 +0530

    Commiting api test5

--- tests/api-tests/get-bookingid-api.spec.ts
+++ tests/api-tests/get-bookingid-api.spec.ts
@@ -1,10 +1,10 @@
-import {test, expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test('Get Booking details by ID - path parameter', async({request}) => {
+test('Get Booking details by ID - path parameter', async ({ request }) => {
 
     const bookingID = 2; //pass booking ID as path parameter
     const response = await request.get(`https://restful-booker.herokuapp.com/booking/${bookingID}`);
-    
+
     //Parse response and print
     const responseBody = await response.json();
     console.log('Get Response:', responseBody);
@@ -12,5 +12,33 @@ test('Get Booking details by ID - path parameter', async({request}) => {
     //Add assertions
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
+
+    //Check that response is not empty
+    expect(responseBody).toHaveProperty("firstname");
+    expect(responseBody).toHaveProperty("lastname");
+
+    expect(responseBody).toHaveProperty("totalprice");
+    expect(typeof responseBody.totalprice).toBe("number");
+    
+
+});
+
+test('Get Booking details by Name - query parameter', async ({ request }) => {
+
+    const firstname = "Mary"; 
+    const lastname = "Jones";
+
+    //send request with query params
+    const response = await request.get(
+        "https://restful-booker.herokuapp.com/booking", { params: { firstname, lastname } });
+
+    //Parse response and print
+    const responseBody = await response.json();
+    console.log('Get Response:', responseBody);
+
+    //Add assertions
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toBe(200);
+
 
 });
